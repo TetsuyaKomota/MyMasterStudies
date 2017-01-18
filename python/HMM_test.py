fed27ded58223c6fb4e8772491448a03d54bbea0
unknown <>
2017-01-18 16:19:11 +0900

    円とシグモイドくっつけてやってみた．うん．

--- python/HMM_test.py
+++ python/HMM_test.py
@@ -14,15 +14,21 @@ import matplotlib.pyplot as plt
 # データを適当に作る
 
 def testfunc_circle(x):
-    return [np.sin(x), np.cos(x)]
+    scale = 200
+    return [scale * np.sin(x), scale * np.cos(x)]
 
 def testfunc_cubic(x):
     a = 1
     b = -10
     c = -1
     d = 1
-
+    
     return [x, a*x*x*x + b*x*x + c*x + d]
+
+def testfunc_sigmoid(x):
+    scale = 200
+    range = 50
+    return [x, scale*(1 - 1/(1+np.exp(-(x-300)/range)))]
 
 
 def makeData(func, size, start, end, noize):
@@ -42,28 +48,30 @@ def makeData(func, size, start, end, noize):
             temp[j] = temp[j] + ep
         output.append(copy.deepcopy(temp))
         x = x + delta
-    
-    
-    
+        
     return output
 
 if __name__ == "__main__":
     datas = []
+    datas.extend(makeData(testfunc_circle, 100, 0, 2*np.pi, 10))
+    datas.extend(makeData(testfunc_sigmoid, 100, 0, 600, 10))
+    # datas.extend(makeData(testfunc_cubic, 100, 0, 2*np.pi, 0.1))
+
     for _ in range(10):
-        # data = makeData(testfunc_circle, 100, 0, 2*np.pi, 0.02)
-        data = makeData(testfunc_cubic, 100, 0, 2*np.pi, 0.05)
-        print(data)
-        datas.append(np.array(data))
+        # datas.extend(makeData(testfunc_circle, 100, 0, 2*np.pi, 10))
+        # datas.extend(makeData(testfunc_cubic, 100, 0, 2*np.pi, 0.1))
+        print(datas)
+        # datas.append(np.array(data))
         # 転置
-        invdata = np.array(data).T
+        invdata = np.array(datas).T
         print(invdata)
         
         plt.plot(invdata[0], invdata[1])
     plt.show()
     
-    model = hmm.GaussianHMM(n_components=80, covariance_type="full")    
+    model = hmm.GaussianHMM(n_components=3, covariance_type="full")    
     
-    model.fit(datas[0])
+    model.fit(datas[0:200])
 
     print("startprob_")
     print(model.startprob_)
