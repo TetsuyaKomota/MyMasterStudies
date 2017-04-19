800c503496747f90080a68533e61b799c45253ff
unknown <>
2017-04-19 18:15:35 +0900

    HMM の符号化結果による境界と本当の境界の差分をヒストグラムで求める experiment_1 を作成

--- python/HMM_test.py
+++ python/HMM_test.py
@@ -6,11 +6,14 @@ Created on Wed Jan 18 13:56:15 2017
 """
 
 import numpy as np
+import time
 from hmmlearn import hmm
 import scipy.stats as ss
 import copy
 import matplotlib.pyplot as plt
 
+
+# ====================================================================================
 # データを適当に作る
 
 def testfunc_circle(x):
@@ -50,8 +53,11 @@ def makeData(func, size, start, end, noize):
         x = x + delta
         
     return output
+# ====================================================================================
+# テスト関数
 
-if __name__ == "__main__":
+    # とりあえず試してみるだけの奴．
+def experiment_0():
     datas = []
     datas.extend(makeData(testfunc_circle, 100, 0, 2*np.pi, 10))
     datas.extend(makeData(testfunc_sigmoid, 100, 0, 600, 10))
@@ -69,10 +75,12 @@ if __name__ == "__main__":
         plt.plot(invdata[0], invdata[1])
     plt.show()
     
-    model = hmm.GaussianHMM(n_components=3, covariance_type="full")    
+    model = hmm.GaussianHMM(n_components=30, covariance_type="full")    
     
     model.fit(datas[0:200])
-
+    
+    '''
+    # 学習結果を表示
     print("startprob_")
     print(model.startprob_)
     print("means_")
@@ -81,10 +89,70 @@ if __name__ == "__main__":
     print(model.covars_)
     print("transmat_")
     print(model.transmat_)    
-    
+    '''
+
+    # 推定.実際の変わり目である100番目,101番目にカッコを付ける
+    pre = model.predict(datas[0:200])
+    print(pre[0:100])
+    print("(", end="")
+    print(pre[100:102], end="")
+    print(")")
+    print(pre[102:])
+    '''
+    # 学習したモデルからサンプリングしてみる
     for _ in range(10):
-        sampleX, sampleZ = model.sample(100)
+        sampleX, sampleZ = model.sample(200)
         sample = np.array(sampleX).T
         plt.plot(sample[0], sample[1])
     plt.show()
-    
+    '''   
+
+    # 符号化後の境界と本当の境界との距離をヒストグラムで表示する関数
+def experiment_1(n_components, dataLength, div):
+    result = []
+    result.append(0)
+    for _ in range(100):
+        # データを生成
+        datas = []
+        datas.extend(makeData(testfunc_circle, div, 0, 2*np.pi, 10))
+        datas.extend(makeData(testfunc_sigmoid, dataLength - div, 0, 600, 10))
+        # datas.extend(makeData(testfunc_cubic, 100, 0, 2*np.pi, 0.1))
+        # モデルを生成
+        model = hmm.GaussianHMM(n_components=n_components, covariance_type="full")    
+        model.fit(datas[0:dataLength])
+        # 符号化
+        res = model.predict(datas[0:dataLength])
+        # 境界の場所を探す 
+        idx = div
+        tempMin = -1
+        while True:
+            if res[idx] != res[idx-1]:
+                tempMin = abs(div - idx)
+                break
+            else:
+                idx = idx - 1
+            #
+        #
+        idx = div
+        while True:
+            if res[idx] != res[idx+1]:
+                tempMin = min(tempMin, abs(div - idx+1))
+                break
+            else:
+                idx = idx + 1
+            #
+        #
+        # ヒストグラムに加える
+        result.append(tempMin)
+    #
+    # ヒストグラムを書く
+    print(result)
+    plt.hist(result, bins = dataLength)
+    plt.show()
+    print("Successly terminated.")
+
+
+
+if __name__ == "__main__":
+    # experiment_0()
+    experiment_1(30, 200, 100)
