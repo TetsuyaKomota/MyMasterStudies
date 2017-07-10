a154fcc79da808dc2243b61052aece9c2d12d487
komota <komotatit@gmail.com>
2017-07-11 03:03:00 +0900

    [feature - reading] バイグラム特徴追加，パラメータ調整，ファイル出力に変更

--- python/reading/PokemonTypeAnalizer.py
+++ python/reading/PokemonTypeAnalizer.py
@@ -1,11 +1,15 @@
 #-*- coding: utf-8 -*-
 
 import dill
+from datetime import datetime
 
 # 可変長ngram を用いて，各タイプごとの部分文字列の出現確率の積でタイプを識別する
 # まずは簡単に 1文字まで分解しよう
 
-ALPHA = 0.1
+ALPHA = 1.0e-12
+# ALPHA = 0.1
+
+NGRAM = 2
 
 class TypeModel:
     def __init__(self, typename):
@@ -69,16 +73,31 @@ class TypeModel:
     def inputName(self, name):
         self.count = self.count + 1
         encoded = ""
+        buff = ""
         for S in name:
+            # 同じ行の文字は揃えてやってみたがあまりよくなかったので，
+            # とりあえずそのまま使うことにする
             # s = self.encodeChar(S)
             s = S
             encoded = encoded + s
-            # s = S
-            if s != "NC" and s in self.hist.keys():
+            # バイグラムもついでに保存してみよう
+            # バイグラムええやん．トライグラムもやってみようか
+            # というか指定した深さまで試せるようにしよう
+            buff = buff + s
+            for l in range(2, NGRAM + 1):
+                if len(buff) > l:
+                    buffL = buff[-1*l:]
+                    if buffL in self.hist.keys():
+                        self.hist[buffL] = self.hist[buffL] + 1
+                    else:
+                        self.hist[buffL] = 1
+            if s == "NC":
+                continue
+            if s in self.hist.keys():
                 self.hist[s] = self.hist[s] + 1
             else:
                 self.hist[s] = 1
-        print(name + "->" + encoded)
+        print(name + "  \t->\t" + encoded)
 
     # 配列で代入すると全部の名前代入する君
     def inputNameList(self, namelist):
@@ -88,13 +107,25 @@ class TypeModel:
     # 事後確率計算
     def calcProbability(self, name):
         output = 1
+        buff = ""
         for S in name:
+            # 同じ行の文字は揃えてやってみたがあまりよくなかったので，
+            # とりあえずそのまま使うことにする
             # s = self.encodeChar(S)
             s = S
+            buff = buff + s
             if s in self.hist.keys():
-                output = output * ((self.hist[s]+ALPHA)/self.getNumofChars())
+                output = output * ((self.hist[s])/(self.getNumofChars()+ALPHA))
             else:
-                output = output * (ALPHA/self.getNumofChars())
+                output = output * (ALPHA/(self.getNumofChars()+ALPHA))
+            for l in range(2, NGRAM + 1):
+                if len(buff) > l:
+                    buffL = buff[-1*l:]
+                    if buffL in self.hist.keys():
+                        output = output * ((self.hist[buffL])/(self.getNumofChars()+ALPHA))
+                    else:
+                        output = output * (ALPHA/(self.getNumofChars()+ALPHA))
+                    
         return output
 
 # 各タイプのモデルを総括する学習モデルクラス
@@ -118,8 +149,8 @@ class PHist:
     # 名前を引数にタイプを推定する
     # 引数 theta は閾値．これを下回るとタイプ2は なし になる
     # 閾値は値よりもタイプ1に対する割合とかの方がよさそう
-    def predict(self, name, theta=1.0e-06):
-        # ピカチュウの確率を計算させてみよう
+    def predict(self, name, theta=0.5, out=None):
+    # def predict(self, name, theta=1.0e-06, out=None):
         types  = []
         scores = []
         types.append("")
@@ -139,12 +170,17 @@ class PHist:
             elif score > scores[1]:
                 types[1] = m
                 scores[1] = score
-        if scores[1] < theta:
+        if (scores[1]/scores[0]) < theta:
+        # if scores[0] < theta:
             types[1] = "なし"
             scores[1] = theta
         print("Predicted type of "+name)
         print("("+types[0]+":"+str(scores[0])+")")
         print("("+types[1]+":"+str(scores[1])+")")
+        if out != None:
+            out.write("Predicted type of "+name+"\n")
+            out.write("("+types[0]+":"+str(scores[0])+")\n")
+            out.write("("+types[1]+":"+str(scores[1])+")\n")
         return {"types":types, "scores":scores}
 
 if __name__ == "__main__":
@@ -176,35 +212,51 @@ if __name__ == "__main__":
     with open("type_names_dict.dill", "rb") as f:
         datas = dill.load(f)
     """
-    print("hello")
     # 各タイプのモデルを生成してみよう
     phist = PHist()
     phist.fit(datas)
     # どの程度正解するか見てみよう
-    with open("pokemon.csv", "r", encoding="utf-8") as f:
-        line = f.readline().split(",")
-        count = 0
-        success_fir = 0
-        success_sec = 0
-        success_rev = 0
-        while len(line) > 2:
-            predicted = phist.predict(line[0])["types"]
-            expected  = [line[2], line[3]]
-            count = count + 2
-            if predicted[0] == expected[0]:
-                success_fir = success_fir + 1
-            if predicted[1] == expected[1]:
-                success_sec = success_sec + 1
-            if predicted[0] == expected[1]:
-                success_rev = success_rev + 1
-            if predicted[1] == expected[0]:
-                success_rev = success_rev + 1
+    with open("result/result" + str(int(datetime.now().timestamp())) + ".txt", "w", encoding="utf-8") as res:
+        with open("pokemon.csv", "r", encoding="utf-8") as f:
             line = f.readline().split(",")
-    print("result:")
-    print(str(success_fir + success_sec) + "/" + str(count))
-    print("fir:" + str(success_fir) + "  sec:" + str(success_sec))
-    print("rev:" + str(success_rev))
-    # 実在しないポケモンのタイプを予想しよう
-    phist.predict("ヒノアラシ")
-    phist.predict("ワニノコ")
-    phist.predict("チコリータ")
+            count = 0
+            success_fir = 0
+            success_sec = 0
+            success_rev = 0
+            while len(line) > 2:
+                predicted = phist.predict(line[0], out=res)["types"]
+                expected  = [line[2], line[3]]
+                count = count + 2
+                if predicted[0] == expected[0]:
+                    success_fir = success_fir + 1
+                if predicted[1] == expected[1]:
+                    success_sec = success_sec + 1
+                # 順番は間違えているけどタイプを正解している個数
+                # 確認のために, どのポケモンのどのタイプだったか出力してみる
+                if predicted[0] == expected[1]:
+                    # print("rev sample:")
+                    # print(line[0] + " : " + str(predicted[0]) + "predicted:0, expected:1")
+                    success_rev = success_rev + 1
+                if predicted[1] == expected[0]:
+                    # print("rev sample:")
+                    # print(line[0] + " : " + str(predicted[1]) + "predicted:1, expected:0")
+                    success_rev = success_rev + 1
+                line = f.readline().split(",")
+        print("result:")
+        print(str(success_fir + success_sec + success_rev) + "/" + str(count))
+        print("fir:" + str(success_fir) + "  sec:" + str(success_sec))
+        print("rev:" + str(success_rev))
+        res.write("result:\n")
+        res.write(str(success_fir + success_sec + success_rev) + "/" + str(count) + "\n")
+        res.write("fir:" + str(success_fir) + "  sec:" + str(success_sec) + "\n")
+        res.write("rev:" + str(success_rev) + "\n")
+        # 実在しないポケモンのタイプを予想しよう
+        phist.predict("チコリータ")
+        phist.predict("ベイリーフ")
+        phist.predict("メガニウム")
+        phist.predict("ヒノアラシ")
+        phist.predict("マグマラシ")
+        phist.predict("バクフーン")
+        phist.predict("ワニノコ")
+        phist.predict("アリゲイツ")
+        phist.predict("オーダイル")
