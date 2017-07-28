e5d3d7e0c2a73bb21651f5c0b7598aba7c61d6e1
unknown <>
2017-07-28 21:02:47 +0900

    [feature - GettingIntermediates.py] 取得したステップのデータのみを抽出してtmp/log_MakerMain/GettingIntermediated/log... に保存するようにした

--- python/HPYLM/tasks/GettingIntermmediates.py
+++ python/HPYLM/tasks/GettingIntermmediates.py
@@ -32,20 +32,18 @@ datas_HMM_naive = u
 # 境界部分のステップ番号のリストを作る
 output = {}
 
-# HPYLM の結果が dict でなく list なので，
-# for 文のインデックスを昇順に渡すよう配慮する必要がある
-l = list(datas_HMM_naive)
-l.sort()
-for i, d in enumerate(l):
+for i, d in enumerate(datas_HMM_naive):
     print(d)
-    m = []
+    m = [1]
     naive = datas_HMM_naive[d]
-    sentense = datas_HPYLM[i]
+    sentence = datas_HPYLM[d]
+    # print(naive)
+    # print(sentence)
     step = 0
-    for word in sentense:
+    for word in sentence:
         for c in word:
             while True:
-                if naive[step] == c:
+                if step < len(naive) and naive[step] == c:
                     step = step + 1
                 else:
                     break
@@ -53,3 +51,21 @@ for i, d in enumerate(l):
     output[d] = m
 # とりあえず表示してみる
 print(output)
+
+# dill.dump しておく
+with open("tmp/log_MakerMain/dills/Getting_intermediates_results.dill", "wb") as f:
+    dill.dump(output, f)
+
+# log ファイルから境界ステップの情報だけを切り出した log データを作成
+for d in output:
+    with open("tmp/log_MakerMain/" + d + ".csv", "r") as f:
+        with open("tmp/log_MakerMain/GettingIntermediated/" + d + ".csv", "w") as g:
+            for s in output[d]:
+                while True:
+                    line = f.readline().split("\n")[0]
+                    if line == "":
+                        break
+                    if line.split(",")[0] == str(s):
+                        g.write(line + "\n")
+                        break
+
