ea316548c3bc87048fb5b1be4920fadb38e78e15
komota <komotatit@gmail.com>
2017-09-17 18:49:17 +0900

    [fix - reading] 修正

---
+++ python/reading/pakistan/arewoHMMniButikomu.py
@@ -0,0 +1,118 @@
+# coding = utf-8
+
+# 1. HMM で学習する
+# 2. 最終状態に対して，次の遷移隔離が最も高い状態を取得する
+# 3. 取得した状態からの出力確率分布から緯度経度を推定する
+
+import warnings
+
+from hmmlearn import hmm
+import numpy as np
+import random
+
+STATE = 10
+SIZE  = 20
+
+bestState = 0
+bestSize  = 0
+bestOK    = 0
+
+res = open("tmp/result.csv", "w", encoding="utf-8")
+
+for st in range(10, 21):
+    for si in range(1, 20):
+        STATE = st
+        SIZE  = 5*si
+        print("step:(state, size)=("+str(STATE)+","+str(SIZE)+")")
+        with warnings.catch_warnings():
+        # for _ in range(1):
+            warnings.simplefilter("ignore")
+
+            model = hmm.GaussianHMM(STATE, covariance_type="full") 
+
+            datas = []
+
+            with open("tmp/are.csv", "r", encoding="utf-8") as f:
+                while True:
+                    line = f.readline()[:-1].split(",")
+                    if len(line) < 2:
+                        break
+                    elif len(line) != 4:
+                        print("おまわりさんこいつです!"+str(line))
+                        continue
+                    datas.append(np.array([float(line[2]), float(line[3])]))
+
+            # print(datas)
+
+            # size で分割
+            size = SIZE
+            splitdatas = []
+            temp = [0]+datas[:size-1]
+            for i in range(size, len(datas)):
+                temp = temp[1:]
+                temp.append(datas[i])
+                splitdatas.append(temp)
+
+            """
+            for s in splitdatas:
+                print(s)
+            """
+            random.shuffle(splitdatas)
+
+            lengths = [size for _ in range(size)]
+
+            pre = []
+            for _ in range(10):
+                try:
+                    model.fit(np.concatenate(splitdatas), lengths)
+                    """
+                    print("startprob_")
+                    print(model.startprob_)
+                    print("means_")
+                    print(model.means_)
+                    print("covars_")
+                    print(model.covars_)
+                    print("transmat_")
+                    print(model.transmat_)    
+
+                    print("------------------")
+                    for s in splitdatas:
+                        print(np.exp(model.score(s)))
+                    """
+                    pre = model.predict(datas)
+                    break
+                except:
+                    print("Error...")
+
+            # print(pre)
+             
+            if pre == []:
+                res.write(str(STATE)+","+str(SIZE)+","+str(0)+"\n")
+                continue 
+            ok = 0
+            ng = 0 
+            for i in range(len(pre)-1):
+                # 遷移確率で推定
+                t = model.transmat_[pre[i]]
+                tempmax = 0
+                for i, T in enumerate(t):
+                    if tempmax < T:
+                        q = i
+                        tempmax = T
+
+                if pre[i+1] == q:
+                    ok += 1
+                else:
+                    ng += 1
+            print("result:(ok,ng)=("+str(ok)+","+str(ng)+")")
+            res.write(str(STATE)+","+str(SIZE)+","+str(ok)+"\n")
+            if ok > bestOK:
+                bestState = st
+                bestSize = si
+                bestOK = ok
+        res.close()
+        res = open("tmp/result.csv", "w", encoding="utf-8")
+print("bestSTATE:" + str(bestState))
+print("bestSIZE :" + str(bestSize))
+print("bestOK   :" + str(bestOK))
+
--- python/reading/pakistan/test.py
+++ python/reading/pakistan/test.py
@@ -3,6 +3,9 @@
 # とりあえずデータを見てみよう
 # 自爆テロを時系列順に並べて，発生場所を HMM で予測してみよう
 
+import re 
+
+
 def readCSV(path):
     output = {}
     index = []
@@ -18,6 +21,54 @@ def readCSV(path):
                 output[l].append(line.split(",")[i])
     return output
 
+# Date がうんこなので書き換える
+def changeDate(datas):
+    c = []
+    tbl = {}
+    tbl["January"] = 1
+    tbl["Jan"] = 1
+    tbl["February"] = 2
+    tbl["Feb"] = 2
+    tbl["March"] = 3
+    tbl["Mar"] = 3
+    tbl["April"] = 4
+    tbl["Apr"] = 4
+    tbl["May"] = 5
+    tbl["May"] = 5
+    tbl["June"] = 6
+    tbl["Jun"] = 6
+    tbl["July"] = 7
+    tbl["Jul"] = 7
+    tbl["August"] = 8
+    tbl["Aug"] = 8
+    tbl["September"] = 9
+    tbl["Sep"] = 9
+    tbl["October"] = 10
+    tbl["Oct"] = 10
+    tbl["November"] = 11
+    tbl["Nov"] = 11
+    tbl["December"] = 12
+    tbl["Dec"] = 12
+    for i,d in enumerate(datas["Date"]):
+        print(i)
+        d = re.sub(" ", "-", d)
+        # 曜日-月-日-年 となっている
+        ye = int(d.split("-")[-1])
+        mo = tbl[d.split("-")[1]]
+        da = int(d.split("-")[2])
+        c.append(int("{0:04d}".format(ye)+"{0:02d}".format(mo)+"{0:02d}".format(da)))
+    datas["Date"] = c
+    return datas
 
 if __name__ == "__main__":
-    print(readCSV("tmp/data.csv"))
+    data = readCSV("tmp/data.csv")
+    data = changeDate(data)
+    size = len(data["S#"])
+    temp = 0
+    for s in range(size):
+        if temp > data["Date"][s]:
+            print("しね")
+            print(data["Date"][s])
+            break
+        temp = data["Date"][s]
+        print(str(s) + "\t:" + str(data["Date"][s]) + "\t\t\t\t:" + data["Latitude"][s] + "\t\t\t\t:" + data["Longitude"][s])
