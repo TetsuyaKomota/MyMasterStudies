490c60f7f8c557454ea7b76e2b725251274c4465
komota <komotatit@gmail.com>
2017-09-29 06:13:03 +0900

    [fix - DPM] 王さん用

--- python/DPM/DPGMM_sklearn.py
+++ python/DPM/DPGMM_sklearn.py
@@ -4,12 +4,15 @@ from sklearn import mixture
 import dill
 import matplotlib.pyplot as plt
 import dill
+import scipy.io
 
 model = mixture.BayesianGaussianMixture(n_components = 10, n_init=150, weight_concentration_prior=0.1)
 
 # data = scipy.io.loadmat("Inputdata.mat")["Inputsample"]
-with open("tmp/log_MakerMain/dills/test_pov.dill", "rb") as f:
-    data = dill.load(f)
+# data = scipy.io.loadmat("TrainingSamples.mat")["Inputsample"]
+data = scipy.io.loadmat("TrainingSamples.mat")["Signals"]
+# with open("tmp/log_MakerMain/dills/test_pov.dill", "rb") as f:
+#     data = dill.load(f)
 
 model.fit(data)
 pre = model.predict(data)
@@ -17,7 +20,6 @@ print(set(pre))
 print(pre)
 print(model.get_params())
 
-"""
 with open("tmp/output.txt", "w", encoding="utf-8") as f:
     print("Labels:")
     print(pre)
@@ -42,7 +44,6 @@ with open("tmp/output.txt", "w", encoding="utf-8") as f:
     f.write("Covariances:\n")
     f.write(str(model.covariances_))
     f.write("\n")
-"""
 
 # プロット
 ci = 0
@@ -54,7 +55,8 @@ for k in set(pre):
         if pre[i] == k:
             Y[0].append(x[0])
             Y[1].append(x[1])
-    col = [((k*20)%255)/255,((k*30)%255)/255,((k*50)%255)/255]
+    K = k+3
+    col = [((K*20)%255)/255,((K*30)%255)/255,((K*50)%255)/255]
     plt.scatter(Y[0], Y[1], c=col)
     ci += 1
 plt.show()
