a450a13b4973407018e02d0416674beccad14320
komota <komotatit@gmail.com>
2017-10-15 22:50:01 +0900

    [feature - gan.py] MNIST 生成器完成

--- python/reading/servalGenerator/gan.py
+++ python/reading/servalGenerator/gan.py
@@ -1,7 +1,8 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Reshape
 from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import UpSampling2D, Convolution2D
+from keras.layers.convolutional import UpSampling2D
+from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers import Flatten, Dropout
 import math
@@ -17,30 +18,30 @@ GENERATED_IMAGE_PATH = "tmp/" # 生成画像の保存先
 
 def generator_model():
     model = Sequential()
-    model.add(Dense(input_dim=100, output_dim=1024))
+    model.add(Dense(1024, input_shape=(100,)))
     model.add(BatchNormalization())
     model.add(Activation("relu"))
-    model.add(Dense(128*7*7))
+    model.add(Dense(7*7*128))
     model.add(BatchNormalization())
     model.add(Activation("relu"))
-    model.add(Reshape((128, 7, 7), input_shape=(128*7*7,)))
+    model.add(Reshape((7, 7, 128)))
     model.add(UpSampling2D((2, 2)))
-    model.add(Convolution2D(64, 5, 5, border_mode="same"))
+    model.add(Conv2D(64, (5, 5), padding="same"))
     model.add(BatchNormalization())
     model.add(Activation("relu"))
     model.add(UpSampling2D((2, 2)))
-    model.add(Convolution2D(1, 5, 5, border_mode="same"))
+    model.add(Conv2D(1, (5, 5), padding="same"))
     model.add(Activation("tanh"))
     return model
 
 def discriminator_model():
     model = Sequential()
-    model.add(Convolution2D(64, 5, 5,
-                            subsample=(2, 2),
-                            border_mode="same",
-                            input_shape=(1, 28, 28)))
+    model.add(Conv2D(64, (5, 5),
+                    strides=(2, 2),
+                    padding='same',
+                    input_shape=(28, 28, 1))) # ここ注意
     model.add(LeakyReLU(0.2))
-    model.add(Convolution2D(128, 5, 5, subsample=(2, 2)))
+    model.add(Conv2D(128, (5, 5), strides=(2, 2)))
     model.add(LeakyReLU(0.2))
     model.add(Flatten())
     model.add(Dense(256))
@@ -51,27 +52,33 @@ def discriminator_model():
     return model
 
 def combine_images(generated_images):
+
+    # メモ : この時点で generated_images.shape = (32, 28, 28, 1) のはず
+
     total = generated_images.shape[0]
     cols = int(math.sqrt(total))
     rows = math.ceil(float(total)/cols)
-    width, height = generated_images.shape[2:]
+    width, height = generated_images.shape[1:3]
     combined_image = np.zeros((height*rows, width*cols),
                               dtype=generated_images.dtype)
 
     for index, image in enumerate(generated_images):
         i = int(index/cols)
         j = index % cols
-        combined_image[width*i:width*(i+1), height*j:height*(j+1)] = image[0, :, :]
+        combined_image[width*i:width*(i+1), height*j:height*(j+1)] = image[:, :, 0]
     return combined_image
 
 def train():
     (X_train, y_train), (_, _) = mnist.load_data()
     X_train = (X_train.astype(np.float32) - 127.5)/127.5
-    X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1], X_train.shape[2])
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
+
+    # メモ : X_train.shape = (60000, 28, 28, 1)
 
     discriminator = discriminator_model()
     d_opt = Adam(lr=1e-5, beta_1=0.1)
     discriminator.compile(loss="binary_crossentropy", optimizer=d_opt)
+    discriminator.summary()
 
     # generator+discriminator （discriminator部分の重みは固定）
     discriminator.trainable = False
@@ -86,11 +93,14 @@ def train():
 
         for index in range(num_batches):
             noise = np.array([np.random.uniform(-1, 1, 100) for _ in range(BATCH_SIZE)])
+            
+            # メモ : noise.shape = (32(バッチサイズ), 100)
+
             image_batch = X_train[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
             generated_images = generator.predict(noise, verbose=0)
 
             # 生成画像を出力
-            if index % 500 == 0:
+            if index % 100 == 0:
                 image = combine_images(generated_images)
                 image = image*127.5 + 127.5
                 if not os.path.exists(GENERATED_IMAGE_PATH):
@@ -111,4 +121,5 @@ def train():
         generator.save_weights("generator.h5")
         discriminator.save_weights("discriminator.h5")
 
-
+if __name__ == "__main__":
+    train()
