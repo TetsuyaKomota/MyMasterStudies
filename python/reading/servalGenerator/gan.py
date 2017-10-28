03746f0d507d8ebc9d93a8b9718c3d96337ba93e
Komota <komotatit@gmail.com>
2017-10-29 00:27:44 +0900

    [fix - reading] 自宅GPU環境で実行できるようにモデルを簡略化

--- python/reading/servalGenerator/gan.py
+++ python/reading/servalGenerator/gan.py
@@ -22,17 +22,13 @@ from setting import GENERATED_IMAGE_PATH
 def generator_model():
     layerSize = int(IMG_SIZE/16)
     model = Sequential()
-    model.add(Dense(2048, input_shape=(100,)))
+    model.add(Dense(1024, input_shape=(100,)))
     model.add(BatchNormalization())
     model.add(Activation("relu"))
-    model.add(Dense(layerSize*layerSize*1024))
+    model.add(Dense(layerSize*layerSize*512))
     model.add(BatchNormalization())
     model.add(Activation("relu"))
-    model.add(Reshape((layerSize, layerSize, 1024)))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(512, (5, 5), padding="same"))
-    model.add(BatchNormalization())
-    model.add(Activation("relu"))
+    model.add(Reshape((layerSize, layerSize, 512)))
     model.add(UpSampling2D((2, 2)))
     model.add(Conv2D(256, (5, 5), padding="same"))
     model.add(BatchNormalization())
@@ -42,21 +38,25 @@ def generator_model():
     model.add(BatchNormalization())
     model.add(Activation("relu"))
     model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(64, (5, 5), padding="same"))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(UpSampling2D((2, 2)))
     model.add(Conv2D(3, (5, 5), padding="same"))
     model.add(Activation("tanh"))
     return model
 
 def discriminator_model():
     model = Sequential()
-    model.add(Conv2D(128, (5, 5),
+    model.add(Conv2D(64, (5, 5),
                     strides=(2, 2),
                     padding='same',
                     input_shape=(IMG_SIZE, IMG_SIZE, 3))) # ここ注意
     model.add(LeakyReLU(0.2))
-    model.add(Conv2D(256, (5, 5), strides=(2, 2)))
+    model.add(Conv2D(128, (5, 5), strides=(2, 2)))
     model.add(LeakyReLU(0.2))
     model.add(Flatten())
-    model.add(Dense(512))
+    model.add(Dense(256))
     model.add(LeakyReLU(0.2))
     model.add(Dropout(0.5))
     model.add(Dense(1))
@@ -128,7 +128,7 @@ def train():
             generated_images = generator.predict(noise, verbose=0)
 
             # 生成画像を出力
-            if index % 50 == 0:
+            if index % 350 == 0:
                 image = combine_images(generated_images[:12])
                 image = image*127.5 + 127.5
                 if not os.path.exists(GENERATED_IMAGE_PATH):
