a384d16476956d6bb67bea33c0d2a26b957d590e
komota <komotatit@gmail.com>
2017-11-07 20:47:41 +0900

    [feature - VideoEncoder.py] キー操作できるようにした

--- python/pyKinect/VideoEncoder.py
+++ python/pyKinect/VideoEncoder.py
@@ -3,6 +3,13 @@ import cv2
 import numpy as np
 import os
 
+"""
+実行して
+ESC : 終了
+1   : 録画開始．新しい CSV ファイルが作られ，物体位置特定してステップログを保存
+2   : 録画終了．CSV ファイルを閉じる．
+"""
+
 def find_rect_of_target_color(image, color):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL)
     h = hsv[:, :, 0]
@@ -16,7 +23,7 @@ def find_rect_of_target_color(image, color):
         mask[((h > 60) & (h < 130)) & (s > 128)] = 255
     elif color == "yellow":
         mask[((h > 10) & (h < 60)) & (s > 128)] = 255
-    elif color == "black":
+    elif color == "hand":
         mask[(s < 10)] = 255
     _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     rects = []
@@ -32,33 +39,49 @@ if __name__ == "__main__":
     colors["blue"]   = (255, 0, 0)
     colors["green"]  = (0, 255, 0)
     colors["yellow"] = (0, 255, 255)
-    colors["black"]  = (0, 0, 0)
+    colors["hand"]   = (0, 0, 0)
     capture = cv2.VideoCapture(0)
     if os.path.exists("tmp/VideoEncoder_results") == False:
         os.mkdir("tmp/VideoEncoder_results")
     count = 0
+    recFlag = False
     while True:
-        count += 1
-        if os.path.exists("tmp/VideoEncoder_results/"+"{0:06d}".format(count) + ".csv") == False:
+        key = cv2.waitKey(30)
+        if key == 27: # ESC
             break
-    f = open("tmp/VideoEncoder_results/"+"{0:06d}".format(count) + ".csv", "w", encoding="utf-8")
-    while cv2.waitKey(30) < 0:
+        elif key == ord("1") and recFlag == False:
+            recFlag = True
+            step = -1
+            while True:
+                count += 1
+                if os.path.exists("tmp/VideoEncoder_results/"+"{0:06d}".format(count) + ".csv") == False:
+                    break
+            f = open("tmp/VideoEncoder_results/"+"{0:06d}".format(count) + ".csv", "w", encoding="utf-8")
+        elif key == ord("2") and recFlag == True:
+            recFlag = False
+            f.close()
+
         _, frame = capture.read()
-        text = ""
-        for c in colors.keys():
-            rects = find_rect_of_target_color(frame, c)
-            if len(rects) > 0:
-                rect = max(rects, key=(lambda x: x[2] * x[3]))
-            else:
-                rect = [0, 0, 0, 0]
-            if rect[2] * rect[3] > 10000:
-                cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), colors[c], thickness=2)
-            else:
-                rect = [0, 0, 0, 0]
-            text += (str(rect[0]+rect[2]/2)+",")
-            text += (str(rect[1]+rect[3]/2)+",")
+        
+        if recFlag == True:
+            step += 1
+            text = str(step) + ","
+            # ログの順番を指定するために colors.keys() ではなくこの形
+            for c in ["hand", "red", "blue", "green", "yellow"]:
+                rects = find_rect_of_target_color(frame, c)
+                if len(rects) > 0:
+                    rect = max(rects, key=(lambda x: x[2] * x[3]))
+                else:
+                    rect = [0, 0, 0, 0]
+                if rect[2] * rect[3] > 10000:
+                    cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), colors[c], thickness=2)
+                else:
+                    rect = [0, 0, 0, 0]
+                text += (str(rect[0]+rect[2]/2)+",")
+                text += (str(rect[1]+rect[3]/2)+",")
+                text += "0,0,0,0,"
+            f.write(text + "\n") 
         cv2.imshow('red', frame)
-        f.write(text + "\n") 
     capture.release()
     f.close()
     cv2.destroyAllWindows()
