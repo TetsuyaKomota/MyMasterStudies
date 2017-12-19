a3c8e0904a89c78ebe29494cb03e246867201960
komota <komotatit@gmail.com>
2017-12-19 19:36:43 +0900

    [feature - final/EncodeModel] 符号化部分を実装．後処理の実装忘れてたのでそっちやる

--- final/EncodeModelwithSOINN.py
+++ final/EncodeModelwithSOINN.py
@@ -13,3 +13,78 @@
 #   tmp/log にあるデータを全部符号化
 #   できた符号列を，ファイル名キーの dict にする
 #   それを dill.dump
+
+from SOINN.SOINN_for_python import SOINN
+import dill
+import os
+import glob
+
+step = 3
+soinnN = 2500
+soinnE = 2500
+
+def fit():
+    filepaths = glob.glob("tmp/log/*.csv")
+    datas = []
+    
+    for filepath in filepaths:
+        # step ステップ幅を切り出す
+        temp = [0 for _ in range(step*2)]
+        with open(filepath, "r", encoding="utf-8") as f:
+            while True:
+                line = f.readline()
+                if line == "":
+                    break
+                # hand の速度成分の部分だけ取り出す
+                # str なので float にキャスト
+                pos = [float(p) for p in line.split(",")[3:5]]
+                # temp を更新
+                temp = temp[2:] + pos
+                datas.append(temp)
+
+    # SOINN を学習
+    soinn = SOINN(step * 2, soinnN, soinnE, n_iter=1, noise_var=0)
+    soinn.fit(X)
+    if os.path.exists("tmp/dills") == False:
+        os.mkdir("tmp/dills")
+    with open("tmp/dills/soinn.dill", "wb") as f:
+        dill.dump(soinn, f)
+
+    return soinn
+
+def predict():
+    if os.path.exists("tmp/dills/soinn.dill") == True:
+        with open("tmp/dills/soinn.dill", "rb") as f:
+            soinn = dill.load(f)
+    else:
+        soinn = fit()
+
+    output = {}
+    for filepath in filepaths:
+        # step ステップ幅を切り出す
+        Z = []
+        Z.append([0 for _ in range(step*2)])
+        with open(filepath, "r", encoding="utf-8") as f:
+            while True:
+                line = f.readline()
+                if line == "":
+                    break
+                # hand の速度成分の部分だけ取り出す
+                # str なので float にキャスト
+                pos = [float(p) for p in line.split(",")[3:5]]
+                # temp を更新
+                Z.append(Z[-1][2:]+pos)
+        
+        # Z[0] はダミーなので消しておく
+        Z.pop(0)
+
+        # 推定
+        output[os.path.basename(filepath)[:-4]] = soinn.classifier(Z)
+
+    with open("tmp/dills/encoded.dill", "wb") as f:
+        dill.dump(output, f)
+
+    return output
+
+if __name__ == "__main__":
+    predict()
