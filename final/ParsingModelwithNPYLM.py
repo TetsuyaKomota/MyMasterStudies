d76bbfaaad893d20788a09777b43af106a7cdf2a
komota <komotatit@gmail.com>
2017-12-19 23:23:08 +0900

    [feature - final/ParsingModel] NPYLM による形態素解析を追加．SOINN が終わってないので未テスト

--- final/ParsingModelwithNPYLM.py
+++ final/ParsingModelwithNPYLM.py
@@ -14,3 +14,108 @@
 #   tmp/dills/encoded.dill で log の全部の符号列をロード
 #   全部を分かち書き
 #   それを dill.dump
+
+import dill
+import os
+from models import RefactedRestaurant
+
+D      = 1
+A      = 5
+Theta  = 1
+PAD    = 1
+MIN_W  = 9
+n_iter = 200
+
+def fit():
+    with open("tmp/dills/encoded.dill", "rb") as f:
+        encoded = dill.load(f)
+
+    rest =RefactedRestaurant.Franchise(D, A, Theta, PAD, MIN_W, 2)
+    
+    # 文字列化
+    strDict = {}
+    for filename in encoded.keys():
+        line = ""
+        for s in encoded[filename]:
+            line += rest.translate(s)
+        strDict[filename] = line
+
+    # 冗長性を削除
+    strDictZip = {}
+    for filename in strDict.keys():
+        t = strDict[filename]
+        strDictZip[filename] = \
+            [t[i] for i in range(len(t)) if i == 0 or t[i] != t[i-1]]
+        
+        # [char] -> str -> [str]
+        strDictZip[filename] = \
+            ["".join(strDictZip[filename])]
+
+    # 学習
+    result = rest.executeParsing(strDictZip, n_iter)
+    
+    # return
+    with open("tmp/dills/npylm.dill", "wb") as f:
+        dill.dump(rest, f)
+    
+    return rest
+
+def parsing():
+    if os.path.exists("tmp/dills/npylm.dill") == True:
+        with open("tmp/dills/npylm.dill", "rb") as f:
+            rest = dill.load(f)
+    else:
+        rest = fit()
+
+    with open("tmp/dills/encoded.dill", "rb") as f:
+        encoded = dill.load(f)
+
+    # 文字列化
+    strDict = {}
+    for filename in encoded.keys():
+        line = ""
+        for s in encoded[filename]:
+            line += rest.translate(s)
+        strDict[filename] = line
+
+    # 冗長性を削除
+    strDictZip = {}
+    for filename in strDict.keys():
+        t = strDict[filename]
+        strDictZip[filename] = \
+            [t[i] for i in range(len(t)) if i == 0 or t[i] != t[i-1]]
+        
+        # [char] -> str -> [str]
+        strDictZip[filename] = \
+            ["".join(strDictZip[filename])]
+
+    # 分割をサンプリング
+    result = rest.executeParsing(strDictZip, n_iter, fit=False)
+
+    # 境界列に変換
+    output = {}
+    for i, d in enumerate(strDict.keys()):
+        print(d)
+        m = [0]
+        naive = strDict[d]
+        sentence = result[d]
+        step = 0
+        for word in sentence:
+            for c in word:
+                while True:
+                    if step < len(naive) and naive[step] == c:
+                        step += 1
+                    else:
+                        break
+            m.append(step)
+        output[d] = m
+    
+    # return 
+    with open("tmp/dills/parsed.dill", "wb") as f:
+        dill.dump(output, f)
+    
+    return output
+
+if __name__ == "__main__":
+    parsing()
+
---
+++ final/models/RefactedRestaurant.py
@@ -0,0 +1,428 @@
+# coding = utf-8
+
+import random
+import numpy as np
+import dill
+import time
+from datetime import datetime
+from copy import deepcopy
+
+# restaurants は各要素 [[テーブル配列], [客配列]]
+# それを識別するための定数
+TABLE    = 0
+CUSTOMER = 1
+
+# テーブル配列の要素は [親文脈のテーブルid, 単語]とする
+# eliminate する際に親文脈から削除するときに必要であるため
+# それを識別するための定数
+T_PID   = 0
+T_WORD  = 1
+
+# 根文脈のテーブルにつける PID
+BASE_PID = -1
+
+# 終始端単語
+PADWORD = "~"
+
+# Pitman-Yor 過程における新要素
+NEW = "THIS CHOICE MEANS NEW FACTOR PICKED PARENT MEASURE"
+
+class Franchise:
+   
+    # A      : 基底測度のパラメータ 
+    # D      : 割引率
+    # THETA  : 強度
+    # PAD    : 終始端単語の数
+    # LEN    : 文脈長さ
+    # MIN_W  : 単語の最短長さ
+    def __init__(self, D, A, THETA, PAD, LEN, MIN_W, isBase=False):
+        self.D           = D
+        self.A           = A
+        self.THETA       = THETA
+        self.PAD         = PAD
+        self.LEN         = LEN
+        self.MIN_W       = MIN_W
+        self.isBase      = isBase
+        # フィールド
+        self.restaurants = {():[[], []]}
+        self.clean       = 0
+        self.timeScore   = {}
+        if isBase == False:
+            # 単語HPYLM なら，文字HPYLM を持つ
+            self.base    = Franchise(D, A, THETA, 3, 2, 1, True)
+
+    def getTheta(self, u):
+        return self.THETA * (len(u) + 1.0)
+
+    def getD(self, u):
+        return (self.D*len(u)) / (len(u)+1.0)
+
+    # テーブル ID 指定でそのテーブルの客のID配列を取得
+    # 客数を知りたいときはこれを len すればいい
+    def getCustomerListofTable(self, u, tableId):
+        rest = self.getRestaurant(u)
+        return [i for i, c in enumerate(rest[CUSTOMER]) if c==tableId]
+
+    def getTableListofWord(self, u, word):
+        rest = self.getRestaurant(u)
+        return [i for i, t in enumerate(rest[TABLE]) if t[T_WORD]==word]
+
+    def getCustomerListofWord(self, u, word):
+        rest = self.getRestaurant(u)
+        customerList = []
+        tids = self.getTableListofWord(u, word)
+        for tid in tids:
+            customerList += self.getCustomerListofTable(u, tid)
+        return customerList
+
+    def addTableofWord(self, u, pid, word):
+        rest = self.getRestaurant(u)
+        rest[TABLE].append([pid, word])
+
+    def addCustomerofTable(self, u, tableId):
+        rest = self.getRestaurant(u)
+        rest[CUSTOMER].append(tableId)
+        # そのテーブルを生成した親文脈のテーブルにも代理客を配置
+        if len(u) > 0:
+            pid = rest[TABLE][tableId][T_PID]
+            self.addCustomerofTable(u[1:], pid)
+
+    def eliminateCustomerofTable(self, u, tableId):
+        rest = self.getRestaurant(u)
+        pid  = rest[TABLE][tableId][T_PID]
+        CustomerList = self.getCustomerListofTable(u, tableId)
+        if len(CustomerList) == 0:
+            print("多分何かがおかしいよ")
+            return
+        delCustomer = CustomerList[0]
+        del rest[CUSTOMER][delCustomer]
+        if len(self.getCustomerListofTable(u, tableId)) == 0:
+            self.eliminateTable(u, tableId)
+        # そのテーブルを生成した親文脈のテーブルから代理客を削除
+        if len(u) > 0:
+            self.eliminateCustomerofTable(u[1:], pid)
+
+    def eliminateTable(self, u, tableId):
+        rest = self.restaurants[u]
+        # その文脈の客のテーブルID を修正
+        for i, c in enumerate(rest[CUSTOMER]):
+            if c > tableId:
+                rest[CUSTOMER][i] -= 1
+        # 子文脈の PID も修正する
+        for cu in [cu for cu in self.restaurants.keys() if cu[1:]==u]:
+            for t in self.restaurants[cu][TABLE]:
+                if t[T_PID] > tableId:
+                    t[T_PID] -= 1
+        del rest[TABLE][tableId]
+
+    # 無駄にあるレストランを削除する
+    def cleanEmptyRestaurant(self):
+        delList = []
+        for u in self.restaurants.keys():
+            if u != () and self.restaurants[u] == [[], []]:
+                delList.append(u)
+        for u in delList:
+            del self.restaurants[u]
+        self.clean = len(self.restaurants)
+
+    # 文脈を取得する．文脈がない場合は生成する
+    def getRestaurant(self, u):
+        # 店数が一定以上の場合，無駄な店をクリーニング
+        if len(self.restaurants) > 2*self.clean:
+            self.cleanEmptyRestaurant()
+        self.addRestaurant(u)
+        return self.restaurants[u]
+
+    def addRestaurant(self, u):
+        # 文脈が LEN より長い場合は生成しない
+        if len(u) > self.LEN:
+            return 
+        # 文脈が存在しないなら新たに生成
+        if u not in self.restaurants.keys():
+            self.restaurants[u] = [[], []]
+
+    # PitmanYor過程から，指定した単語のテーブルID を取得
+    # 単語指定の条件付確率を用いる
+    # 新テーブルが選択される場合は NEW を返す
+    def pickwithPitmanYor(self, u, word):
+        rest = self.getRestaurant(u)
+        tableList = self.getTableListofWord(u, word)
+        tableHist = {NEW:self.getTheta(u)}
+        for tid in tableList:
+            tableHist[tid] = len(self.getCustomerListofTable(u, tid))
+        total = sum(tableHist.values())
+        
+        # サンプリングを行う
+        rand = random.random()
+        temp = 0
+        for tid in tableHist:
+            temp += tableHist[tid]
+            if rand <= temp/total:
+                return tid
+
+    # ある文脈に対して単語指定で客を追加する
+    # この処理は「テーブルを選択する」という Pitman-Yor 過程と
+    # 親文脈に向かう再帰処理が必要
+    def addCustomerofWord(self, u, word, add=True):
+        tid = self.pickwithPitmanYor(u, word)
+        # NEW を引いた場合，親文脈から新テーブルを取得する
+        if tid == NEW:
+            if len(u) == 0:
+                pid = BASE_PID
+            else:
+                # addCustomerofTable が代理客を生成するので，
+                # 新テーブル生成時に代理客を配置しないように
+                # add=False とする
+                pid = self.addCustomerofWord(u[1:], word, add=False)
+            self.addTableofWord(u, pid, word)
+            tid = len(self.restaurants[u][TABLE])-1
+        # 再帰の最初のみ，addCustomerofTable を実行する
+        if add == True:
+            self.addCustomerofTable(u, tid)
+        return tid
+
+    # 文を代入して客を配置
+    # 新実装は全客に対して代理客を生成するので，
+    # LEN の長さに分割してそのまま代入するだけでいい
+    def addSentence(self, sentence):
+        # 単語HPYLM なら,文字HPYLM を更新
+        if self.isBase == False:
+            for w in [s for s in sentence if s != PADWORD]:
+                self.base.addSentence(self.forBase(w))
+
+        part = [PADWORD for _ in range(self.LEN)]
+        for word in sentence:
+            part = part[1:] + [word]
+            # すべてパディングであるような文脈は無視
+            if part == [PADWORD for _ in range(self.LEN)]:
+                continue
+            self.addCustomerofWord(tuple(part[:-1]), part[-1])
+ 
+    # 文を代入して客を削除
+    # 新実装は全客に対して代理客を生成するので，
+    # LEN の長さに分割してそのまま削除するだけでいい
+    # 削除に関しては Pitman-Yor 過程する必要なく，
+    # また add されている前提なので文脈生成も必要なし
+    def eliminateSentence(self, sentence):
+        # 単語HPYLM なら,文字HPYLM を更新
+        if self.isBase == False:
+            for w in [s for s in sentence if s != PADWORD]:
+                self.base.eliminateSentence(self.forBase(w))
+
+        part = [PADWORD for _ in range(self.LEN)]
+        for word in sentence:
+            part = part[1:] + [word]
+            # すべてパディングであるような文脈は無視
+            if part == [PADWORD for _ in range(self.LEN)]:
+                continue
+            tid = self.getTableListofWord(tuple(part[:-1]), part[-1])[0]
+            self.eliminateCustomerofTable(tuple(part[:-1]), tid)
+
+    # 指定した文脈で，指定した単語が生成される確率
+    def calcProbabilityforU(self, u, w):
+        c_uw = len(self.getCustomerListofWord(u, w))
+        c_u  = len(self.restaurants[u][CUSTOMER])
+        t_uw = len(self.getTableListofWord(u, w))
+        t_u  = len(self.restaurants[u][TABLE])
+        D    = self.getD(u)
+        T    = self.getTheta(u)
+        if len(u) == 0:
+            Ppar = self.calcProbabilityofBaseMeasure(w)
+        else:
+            Ppar = self.calcProbabilityforU(u[1:], w)
+        term1 =  (1.0*(c_uw-D*t_uw)) / (T+c_u)
+        term2 = ((1.0*(T+D*t_u)) / (T+c_u)) * Ppar
+        return term1 + term2
+
+    # 基底測度は単語長さの正規分布
+    # 短すぎる単語を無視する項を追加
+    def calcProbabilityofBaseMeasure(self, w):
+        # return np.exp(-len(w) * self.A) * int(len(w)>self.MIN_W)
+        if self.isBase == False:
+            # 単語HPYLM なら，文字HPYLM から取得してくる
+            return self.base.calcProbability(self.forBase(w))
+        else:
+            # 文字HPYLM なら，各文字の一様分布
+            # 根店のテーブルに配置されているのが全文字のはず
+            charset = set([t[1] for t in self.getRestaurant(())[TABLE]])
+            # PADWORD が含まれているので 1 引く
+            return 1.0/(len(charset)-1)
+
+    # 指定した文章の生成確率
+    # 終始端単語は含まれている前提
+    def calcProbability(self, sentence):
+        output = 1
+        part = [PADWORD for _ in range(self.LEN)]
+        for word in sentence:
+            part = part[1:] + [word]
+            # すべてパディングであるような文脈は無視
+            if part == [PADWORD for _ in range(self.LEN)]:
+                continue
+            output *= self.calcProbabilityforU(tuple(part[:-1]), part[-1])
+        return output 
+
+    # 文章と番号を引数に，境界状態を入れ替える
+    def changeBoundary(self, sentence, charsIdx):
+        if charsIdx <= 0 or charsIdx >= sum([len(w) for w in sentence]):
+            return sentence
+        currentNumofChars = 0
+        changedIdx  = -1
+        while currentNumofChars < charsIdx:
+            changedIdx  += 1
+            currentNumofChars += len(sentence[changedIdx])
+
+        o =  sentence[:changedIdx]
+        if currentNumofChars == charsIdx:
+            o += [sentence[changedIdx]+sentence[changedIdx+1]]
+            o += sentence[changedIdx+2:]
+        else:
+            div = charsIdx - (currentNumofChars-len(sentence[changedIdx]))
+            o  += [sentence[changedIdx][:div],sentence[changedIdx][div:]]
+            o  += sentence[changedIdx+1:]
+        return o 
+
+    # サンプリング
+    # fit : boolean : モデルの更新をするか
+    #                 学習時には True, 分割の推定時には False
+    #                 executeParsing の引数で指定する
+    def sampling(self, sentence, fit):
+        timeScore = []
+        # 古い文章を削除
+        if fit == True:
+            self.eliminateSentence(sentence)
+        newSentence = deepcopy(sentence)
+        # 終始端単語を無視
+        length = sum([len(w) for w in sentence if w!=PADWORD])-1
+        idxList = list(range(length))
+        random.shuffle(idxList)
+        # 始端単語を無視するためのオフセット
+        padlen = len(PADWORD)*self.PAD+1
+        for idx in idxList:
+            newSentence_a = self.changeBoundary(newSentence, idx+padlen)
+            p_b = self.calcProbability(newSentence)
+            p_a = self.calcProbability(newSentence_a)
+        if random.random() < p_a / (p_a + p_b):
+            newSentence = newSentence_a
+        # 新しい文章を挿入
+        if fit == True:
+            self.addSentence(newSentence)
+        return newSentence
+
+    # 辞書内の全文章を反転(各単語を反転し，単語の並び自体も反転)
+    def reverseSentences(self, sentenceDict):
+        output = {}
+        for s in sentenceDict:
+            output[s] = []
+            for i in range(len(sentenceDict[s])):
+                output[s].append(sentenceDict[s][-i-1][::-1]) 
+        return output
+
+    # 実行
+    # rev : boolean : 逆向きにした文字列の分割を併用して行うか
+    #                 True にした場合，reverseSentences した文字列も
+    #                 対象に加えて解析を行う
+    #
+    # fit : boolean : モデルの更新を行うか
+    #                 学習時には True, 分割の推定時には False を指定する
+    def executeParsing(self, sentenceDict, n_iter, rev=False, fit=True):
+        current = deepcopy(sentenceDict)
+        if rev == True:
+            current = self.reverseSentences(current)
+        PADS = [PADWORD for _ in range(self.PAD)]
+        for c in current:
+            current[c] = deepcopy(PADS)+current[c]+deepcopy(PADS)
+            self.addSentence(current[c])
+        for i in range(n_iter):
+            for c in current:
+                current[c] = self.sampling(current[c], fit)
+            if i % (int(n_iter/10)) == 0:
+                print("[RefactedRest]executeParsing:iteration:"+str(i))
+                print("[RefactedRest]executeParsing:currentSentences:")
+                for c in sorted(list(current.keys())):
+                    print(c + ":" + str(current[c]))
+        for c in current:
+            current[c] = [w for w in current[c] if w != PADWORD] 
+        if rev == True:
+            current = self.reverseSentences(current)
+        print("[RefactedRest]executeParsing:results:")
+        for c in current:
+            print(c + ":" + str(current[c]))
+        # 最後に掃除する
+        self.cleanEmptyRestaurant()
+        return current
+
+    def debug_start(self, idx):
+        if idx not in self.timeScore.keys():
+            self.timeScore[idx] = []
+        self.timeScore[idx].append(datetime.now().timestamp())
+
+    def debug_endof(self, idx):
+        if idx not in self.timeScore.keys():
+            return 
+        self.timeScore[idx][-1] *= -1
+        self.timeScore[idx][-1] += datetime.now().timestamp()
+
+    def debug_result(self):
+        output = {}
+        for idx in self.timeScore.keys():
+            scores = self.timeScore[idx]
+            output[idx] = sum(scores)/len(scores)
+        return output
+
+    # デバッグ用の出力メソッド
+    def toPrint(self):
+        print("===================")
+        self.sub_toPrint(())
+        print("===================")
+
+    def sub_toPrint(self, u):
+        rest = self.getRestaurant(u)
+        indent = "".join(["\t" for _ in range(len(u))])
+        customers = [rest[TABLE][i][T_WORD] for i in rest[CUSTOMER]]
+        print(indent + "U:" + str(u))
+        print(indent + "W:" + str(customers))
+        childs = [cu for cu in self.restaurants.keys() if cu[1:]==u]
+        for cu in childs:
+            # () が子文脈になるはずはない
+            if len(cu) == 0:
+                continue
+            self.sub_toPrint(cu)
+
+    def translate(self, number):
+        # 61 は 「~」, 62 ~ 95 は特殊文字なので飛ばす
+        if number >= 61:
+            return (chr(ord("A")+number+35))
+        return (chr(ord("A")+number))
+
+    # アルファベットを数字に変換
+    def retranslate(self, alphabet):
+        # 96 以上なら特殊文字回避のため += 36 されてるはず
+        if ord(alphabet) - ord("A") >= 97:
+            return ord(alphabet) - ord("A") - 35
+        return ord(alphabet) - ord("A")
+
+    # 単語を引数に，文字HPYLM に渡すためのリストに変換する
+    def forBase(self, word):
+        output = list(word)
+        pads   = [PADWORD for _ in range(self.base.PAD)]
+        return pads + output + pads
+
+if __name__ == "__main__":
+    f = Franchise(1, 1, 1, 3, 3)
+    data = {}
+    data["1"] = ["りんごぶどうみかんばななもも"]
+    data["2"] = ["ももみかんばななりんごぶどう"]
+    data["3"] = ["ぶどうばななみかんりんごもも"]
+    data["4"] = ["ばななりんごみかんももぶどう"]
+    data["5"] = ["みかんももばななりんごぶどう"]
+
+    for i in range(1):
+        with open("tmp/RefactedRest_result.dill", "wb") as g:
+            ptime = datetime.now().timestamp()
+            dill.dump(f.executeParsing(data, 300), g)
+            ptime = datetime.now().timestamp() - ptime
+
+    f.toPrint()
+    res = f.debug_result()
+    for r in res:
+        print(r + "\t\t: " + str(res[r]))
