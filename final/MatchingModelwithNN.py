58c24924ad06e845f22d26ae6e8f10e0762d4430
komota <komotatit@gmail.com>
2017-12-23 20:09:40 +0900

    [feature - MatchingModel] パーセプトロンでアンサンブル学習するやつ実装中

--- final/MatchingModelwithNN.py
+++ final/MatchingModelwithNN.py
@@ -17,3 +17,94 @@
 # 4. before を output に追加
 # 5. before <- predict
 # 6.1. に戻る
+
+from keras import Sequential
+from keras.layers import Dense, Activation
+import dill
+import numpy as np
+
+e          = 30  # 許容誤差
+n_iter     = 100 # サンプリング学習の回数
+
+# パーセプトロンを生成
+def build(numofInput):
+    model = Sequential()
+    model.add(Dense(numofInput, input_shape=(numofInput, )))
+    model.add(Activation("relu"))
+    model.add(Dense(numofInput))
+    model.add(Activation("linear"))
+    model.compile(optimizer="adam", loss="mean_squared_error")
+    return model
+
+
+def matching():
+    # データのロード
+    with open("tmp/dills/prunned.dill", "rb") as f:
+        prunned = dill.load(f)
+    keys = prunned.keys()
+    size = 0   # データの次元． 2*オブジェクト数
+    datas = {}
+    for filename in keys:
+        datas[filename] = []
+        with open("rnp/log/"+filename+".csv", "r", encoding="utf-8") as f:
+            while True:
+                line = f.readline().split(",")
+                if len(line) < 2:
+                    break
+                if size == 0:
+                    size = len(line[5:-1])
+                datas[filename].append([float(l) for l in line[5:-1]])
+
+    # 共通境界推定
+    output = {}
+    before = {}
+    for filename in keys:
+        before[filename] = prunned[filename][0]
+    while True:
+        after = {}
+        for fn in keys:
+            # before+e より大きい最小の step 
+            after[fn] = [s for s in prunned[fn] if s > before[fn]+e][0]
+
+        # サンプリング学習を n_iter 回行う
+        modelList = []
+        for _ in range(n_iter):
+            # サンプリング
+            model = build(size)
+            sampleList = np.random.choice(list(keys), len(keys))
+            X = []
+            y = []
+            for sample in sampleList:
+                X.append(np.array(datas[sample][before[sample]]))
+                y.append(np.array(datas[sample][after[sample]]))
+            
+            # 学習
+            model.fit(X, y)
+
+            # 評価
+            w = model.evaluate(X, y)[0]
+
+            # 保存
+            modelList.append((model, w))
+
+        # 重みをソフトマックスで付けるために，総和を求めておく
+        sumexp = sum([np.exp(-1.0*m[1]) for m in modelList])
+
+        # 次を推定する
+        predict = {}
+        for filename in keys:
+            predict[filename] = []
+            for m in modelList:
+                beforeData = datas[filename][before[filename]]
+                predict[filename].append(m[0].predict([beforeData])[0])
+                predict[filename][-1] *= np.exp(-1.0*m[1])/sumexp
+            predict[filename] = sum(predict[filename])
+
+        # 推定結果に最も近い状態を datas から取得
+        selected = {}
+        for filename in keys:
+            # TODO ここ頑張れ
+            pass
+        # before を output に追加
+        # before <- selected
+        # 終了条件
