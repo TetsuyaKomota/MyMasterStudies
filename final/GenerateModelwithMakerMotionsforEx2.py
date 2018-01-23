be3235f98414d39333d998f2720c4ab4ad2de965
komota <komotatit@gmail.com>
2018-01-24 00:42:04 +0900

    [add - GeneratorModelforEx2] 実験2用に初期状態を調整したデータセット正瀬尾モデル作成

---
+++ final/GenerateModelwithMakerMotionsforEx2.py
@@ -0,0 +1,118 @@
+# coding = utf-8
+
+# やること
+# データ作る
+
+# メソッド
+# ・データ生成
+#   tmp/log に，指定した動作を生成
+
+# 実験 2 用
+# 各教示動作の初期状態は完全ランダムではなく
+# 直前の動作の初期状態から微妙に変化したもの
+
+import numpy as np
+from numpy.random import choice
+from random import random
+import os
+
+numTrain = 30
+numTest  = 30
+
+def generate(path, num):
+
+    if os.path.exists(path) == False:
+        os.mkdir(path)
+
+    
+    isMoveChangable = True
+
+    # ランダムに初期化
+    objList = ["red", "blue", "green", "yellow"]
+    objNum  = len(objList)
+    rands = list((np.random.rand(objNum*2)-0.5) * 10000)
+    ginits = {"hand" : np.zeros(2)}
+    for o in objList:
+        ginits[o] = np.array([rands.pop(), rands.pop()])
+   
+    # データを作成する
+    for i in range(num):
+        filename = path+"{0:05d}".format(i)+".csv"
+
+        # 初期値を微動
+        rands = list((np.random.rand(objNum*2)-0.5) * 3000)
+        inits = {"hand" : np.zeros(2)}
+        for o in objList:
+            inits[o] = ginits[o] + np.array([rands.pop(), rands.pop()])
+
+        # 動作をランダムに決定
+        # いずれも「取りに行く→動かす→取りに行く→動かす→戻る」なので
+        # 取りに行く物体，動かす先をランダムに決定する
+        # choice の False は「重複不可」
+        if isMoveChangable == True:
+            moves = []
+            moved = choice(objList, 3, False)
+            # moved = choice(objList, 2, False)
+            moves.append(np.array([moved[0]]))
+            moves.append(choice(objList, max(1, choice(objNum+1)), False))
+            moves.append(np.array([moved[1]]))
+            moves.append(choice(objList, max(1, choice(objNum+1)), False))
+            moves.append(np.array([moved[2]]))
+            moves.append(choice(objList, max(1, choice(objNum+1)), False))
+            moves.append(choice(objList, 0, False))
+
+        # test データ生成の場合，moves は初期化以降変更できなくする
+        if "test" in path:
+            isMoveChangable = False
+ 
+        step = 0
+        pick = "none"
+
+        # 動作を行う 
+        with open(filename, "w", encoding="utf-8") as f:
+            for s in range(len(moves)):
+                # hand の目的地(moves[s] の物体位置と 9:1 のところ)を求める
+                subgoal = np.zeros(2)
+                for o in moves[s]:
+                    subgoal += inits[o]
+                if len(moves[s]) > 0:
+                    subgoal /= len(moves[s])
+                subgoal -= inits["hand"]
+
+                # 100 ステップで到達するための初速と加速度を求める
+                v = subgoal/50
+                a = subgoal/(-5000)
+
+                # 100 ステップ実行する
+                for _ in range(100):
+                    # 出力
+                    text  = str(step) + ","
+                    text += str(inits["hand"][0])+","
+                    text += str(inits["hand"][1])+","
+                    text += str(v[0])+","
+                    text += str(v[1])+","
+                    for o in objList:
+                        text += str(inits[o][0])+","+str(inits[o][1])+","
+                    f.write(text+"\n") 
+
+                    # ステップインクリメント
+                    step += 1
+
+                    # 移動
+                    inits["hand"] += v
+                    if pick != "none":
+                        inits[pick] += v
+                    v += a
+
+                # step = 100, 300, 500 なら掴み，200, 400, 600 なら離す
+                # if step in [100, 300]:
+                if step in [100, 300, 500]:
+                    pick = moves[s][0]
+                else:
+                    pick = "none"
+
+if __name__ == "__main__":
+    # train (SOINN, NPYLM 学習用)
+    generate("tmp/log_ex2/", numTrain)
+    # test  (Matching 学習用)
+    generate("tmp/log_ex2_test/", numTest)
