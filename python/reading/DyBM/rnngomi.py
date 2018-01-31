3de996d264f83b607bb1d8662d5cefc440bfccd8
komota <komotatit@gmail.com>
2018-01-31 13:03:02 +0900

    [feature - reading] DyBM コードをけもフレ用に修正

--- python/reading/DyBM/rnngomi.py
+++ python/reading/DyBM/rnngomi.py
@@ -29,14 +29,15 @@ def RMSE(y_true,y_pred):
     """
     return np.sqrt(MSE(y_true,y_pred))
 
-data, samplerate = sf.read("tmp/001.wav")
+# data, samplerate = sf.read("tmp/001.wav")
+data, samplerate = sf.read("tmp/kemono_op.wav")
 timeSeries = []
-for i in range(int(len(data)/3)):
-    timeSeries.append(np.array([data[i]]))
+for i in range(int(len(data)/3), (len(data))):
+    timeSeries.append(np.array(data[i]))
 
 #DyBM initialization parameters
-in_dim = 1        # dimension of the input time-series
-out_dim = 1       # dimension of the expected output time-series
+in_dim = 2        # dimension of the input time-series
+out_dim = 2       # dimension of the expected output time-series
 rnn_dim = 100     # dimension of RNN layer
 max_iter =  1     # maximum number of learning epochs/iterations to run
 SGD = RMSProp     # setting the SGD optimization method  
@@ -66,19 +67,21 @@ for i in range(max_iter):
     error = RMSE(result["actual"],result["prediction"])
     print ('Learning epoch RMSE : %.5f' %(error))
 
+# sf.write("tmp/kemono_002.wav", [r[0] for r in result["prediction"]], samplerate)
+sf.write("tmp/kemono_002.wav", result["prediction"], samplerate)
+
 plt.subplot(2, 1, 1)
 plt.title("First 900")
-plt.plot(result["actual"][:900],label="target")
-plt.plot(result["prediction"][:900],label="prediction")
+plt.plot([a[0] for a in result["actual"]][:900],label="target")
+plt.plot([a[0] for a in result["prediction"]][:900],label="prediction")
 plt.legend()
 plt.subplot(2, 1, 2)
 plt.title("Last  900")
-plt.plot(result["actual"][-900:],label="target")
-plt.plot(result["prediction"][-900:],label="prediction")
+plt.plot([a[0] for a in result["actual"]][-900:],label="target")
+plt.plot([a[0] for a in result["prediction"]][-900:],label="prediction")
 plt.legend()
 plt.show()
     
-sf.write("tmp/002.wav", [r[0] for r in result["prediction"]], samplerate)
 
 # _update_state という内部メソッドが怪しい
 # time_series_model の get_prediction の実装見ると
@@ -98,9 +101,10 @@ for t, tRate in enumerate([2**i for i in range(tRange)]):
             model._update_state(pred)
     plt.subplot(tRange, 1, t+1)
     plt.title("True data in each " + str(tRate) + "step")
-    plt.plot(gen[:900],label="generated")
+    plt.plot([g[0] for g in gen][:900],label="generated")
     plt.legend()
-    sf.write("tmp/004_"+str(tRate)+".wav", [g[0] for g in gen], samplerate)
+    # sf.write("tmp/kemono_004_"+str(tRate)+".wav", [g[0] for g in gen], samplerate)
+    sf.write("tmp/kemono_004_"+str(tRate)+".wav", gen, samplerate)
 
 plt.show()
 """
